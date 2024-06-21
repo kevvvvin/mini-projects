9f03ffe4b49027df41031d9eea44836513dc4a08
Kevin Abelgas <kev.abelgas@gmail.com>
2024-06-21 11:42:20 +0800

    add: clear calculator function, modularize getting user input

--- mini-project-1/Calculator/src/main/java/Calculator.java
+++ mini-project-1/Calculator/src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator {
     private double total;
     private String operation;
@@ -13,13 +15,48 @@ public class Calculator {
         this.isOperatorValid = false;
     }
 
-    // Checks if the user input is a valid number or if user wants to quit
-    public boolean InputIsNumber(String input) {
-        if (input.equalsIgnoreCase("Q")) {
-            TogglePower();
-            return false;
+    public boolean GetNumber(Scanner sc) {
+        while (!this.isNumValid && this.isRunning) {
+            System.out.print("Enter a number: ");
+            String num = sc.next();
+            if (num.equalsIgnoreCase("C")) {
+                this.Clear();
+                return false;
+            }
+            else if (num.equalsIgnoreCase("Q")) {
+                this.TogglePower();
+                return false;
+            }
+            else if (this.InputIsNumber(num)) {
+                this.HandleArithmetic(num);
+                return true;
+            }
         }
+        return false;
+    }
 
+    public boolean GetOperator(Scanner sc) {
+        while (!this.isOperatorValid && this.isRunning) {
+            System.out.print("Enter an operator [+, -, *, /]: ");
+            String operator = sc.next();
+            if (operator.equalsIgnoreCase("C")) {
+                this.Clear();
+                return false;
+            }
+            else if (operator.equalsIgnoreCase("Q")) {
+                this.TogglePower();
+                return false;
+            }
+            else if (this.InputIsOperator(operator)) {
+                this.SetOperation(operator);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Checks if the user input is a valid number or if user wants to quit
+    private boolean InputIsNumber(String input) {
         try {
             double num = Double.parseDouble(input);
             if (num == 0 && this.operation.equals("/")) {
@@ -35,14 +72,11 @@ public class Calculator {
     }
 
     // Checks if the user input is a valid operator or if user wants to quit
-    public boolean InputIsOperator(String input) {
+    private boolean InputIsOperator(String input) {
         switch (input) {
             case "+", "-", "/", "*":
                 this.isOperatorValid = true;
                 return true;
-            case "q", "Q":
-                TogglePower();
-                break;
             default:
                 System.out.println("Not a valid operator.");
                 break;
@@ -51,7 +85,9 @@ public class Calculator {
     }
 
     // Handles arithmetic operations based on operator provided by the user
-    public void HandleArithmetic(String input) {
+    private void HandleArithmetic(String input) {
+        double prevTotal = this.total;
+
         if (this.operation.isEmpty() || this.operation.equals("+")) {
             this.total = Add(this.total, Double.parseDouble(input));
         }
@@ -67,32 +103,40 @@ public class Calculator {
 
         // Prevent printing total value if input is the first number
         if (!this.operation.isEmpty()) {
-            System.out.println("Total: "+ this.total);
+            System.out.println(prevTotal + " " + this.operation + " " + input + " = " + this.total);
         }
     }
 
-    public void TogglePower() {
+    private void SetOperation(String input) {
+        this.operation = input;
+    }
+
+    private void TogglePower() {
         this.isRunning = !this.isRunning;
         System.out.println("Calculator is now off.");
     }
 
-    public void SetOperation(String input) {
-        this.operation = input;
+    private void Clear() {
+        this.operation = "";
+        this.total = 0;
+        this.isNumValid = false;
+        this.isOperatorValid = false;
+        System.out.println("Calculator cleared.");
     }
 
-    public double Add(double x, double y) {
+    private double Add(double x, double y) {
         return x + y;
     }
 
-    public double Subtract(double x, double y) {
+    private double Subtract(double x, double y) {
         return x - y;
     }
 
-    public double Multiply(double x, double y) {
+    private double Multiply(double x, double y) {
         return x * y;
     }
 
-    public double Divide(double x, double y) {
+    private double Divide(double x, double y) {
         return x / y;
     }
 }
--- mini-project-1/Calculator/src/main/java/Main.java
+++ mini-project-1/Calculator/src/main/java/Main.java
@@ -6,27 +6,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Calculator is now on!");
-        System.out.println("Enter [q or Q] to turn off.\n");
+        System.out.println("Enter [q or Q] to turn off.");
+        System.out.println("Enter [c or C] to clear the calculator.\n");
 
         while (calculator.isRunning) {
-            while (!calculator.isNumValid && calculator.isRunning) {
-                System.out.print("Enter a number: ");
-                String num = sc.next();
-                if (calculator.InputIsNumber(num)) {
-                    calculator.HandleArithmetic(num);
-                }
-            }
-
-            while (!calculator.isOperatorValid && calculator.isRunning) {
-                System.out.print("Enter an operator [+, -, *, /]: ");
-                String operator = sc.next();
-                if (calculator.InputIsOperator(operator)) {
-                    calculator.SetOperation(operator);
-                }
-            }
-
+            if (!calculator.GetNumber(sc)) {continue;}
+            if (!calculator.GetOperator(sc)) {continue;}
             calculator.isNumValid = false;
             calculator.isOperatorValid = false;
         }
+
+        sc.close();
     }
 }
